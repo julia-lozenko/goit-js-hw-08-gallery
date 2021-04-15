d39fb4f6821420a554cdc1d731e34594866137b7
Julia Lozenko <jlozenko@gmail.com>
2021-04-15 13:46:14 +0300

    added 8th homework

---
+++ src/js/task-08.js
@@ -0,0 +1,86 @@
+import imagesRefs from "./gallery-items.js";
+
+// - Создание и рендер разметки по массиву данных и предоставленному шаблону.
+
+const galleryContainer = document.querySelector(".js-gallery");
+const modalLightBox = document.querySelector(".js-lightbox");
+const lightBoxImage = document.querySelector(".lightbox__image");
+
+const galleryMarkup = (createGalleryCards(imagesRefs));
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+function createGalleryCards(items) { 
+  return items.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+     <a
+      class="gallery__link"
+      href= "${original}"
+     >
+    <img 
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt= ${description}
+      />
+    </a>
+   </li>
+   `;
+  })
+    .join('');
+
+};
+  
+// - Реализация делегирования на галерее `ul.js-gallery` и получение `url` большого
+//   изображения.
+// Открытие модального окна по клику на элементе галереи.
+// Подмена значения атрибута src элемента img.lightbox__image.
+
+galleryContainer.addEventListener("click", onGalleryElClick);
+
+function onGalleryElClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const imgOriginalUrl = e.target.dataset.source;
+  console.log(e.target);
+  e.target.classList.add("current");
+  
+  modalLightBox.classList.add("is-open");
+  lightBoxImage.src = imgOriginalUrl;
+
+};
+
+// - Закрытие модального окна по клику на кнопку  `button[data-action="close-lightbox"]`.
+
+  // Очистка значения атрибута src элемента img. lightbox__image.Это необходимо для того,
+  // чтобы при следующем открытии модального окна,
+  // пока грузится изображение, мы не видели предыдущее.
+
+const closeBtn = document.querySelector('button[data-action="close-lightbox"]');
+
+closeBtn.addEventListener("click", closeModal);
+
+function closeModal(e) {
+  modalLightBox.classList.remove("is-open");
+
+  lightBoxImage.src = "";
+  const current = document.querySelector(".gallery__image.current");
+  current.classList.remove("current");
+}
+
+// // Закрытие модального окна по клику на div.lightbox__overlay.
+
+const containerOverlay = document.querySelector(".lightbox__overlay");
+containerOverlay.addEventListener("click", closeModal);
+
+// - Закрытие модального окна по нажатию клавиши `ESC`.
+
+window.addEventListener("keydown", onEscPress);
+function onEscPress(e) {
+  if (e.code === "Escape") {
+    closeModal();
+  }
+}
